fde00515e275e8e6bf9eca9f64fce222e88c542d
SAKSHI-BHATI <jainendrabhati007@gmail.com>
2025-09-11 16:59:05 +0530

    code2
    sum of arr

---
+++ 2nd.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int sums=0;
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+        sums = sums+arr[i];
+    }
+    cout<<"arr = "<<"{";
+    for(int i=0;i<n;i++){
+        if (i==n-1){
+            cout<<arr[i];
+            break;
+        }
+        cout<<arr[i]<<" ,";
+    }
+    cout<< "}"<<endl;
+    cout<<"sum is "<<sums;
+}
