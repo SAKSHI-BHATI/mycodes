aee8be6a2ba546ba5ad30e21921117ed5bdca560
SAKSHI-BHATI <jainendrabhati007@gmail.com>
2025-09-12 15:13:18 +0530

    star patterns

---
+++ 1st.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i =1;i<=n;i++){
+        cout<<string(n-i,' ')<<string(i,'#')<<endl;//for char+' ',for large word =" "
+    }
+    return 1;
+}
