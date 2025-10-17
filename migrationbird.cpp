a30cb6fd62261f2a04ba2836911d4582665fbe52
SAKSHI-BHATI <jainendrabhati007@gmail.com>
2025-10-17 23:51:55 +0530

    again corre cting the code after commit

--- migrationbird.cpp
+++ migrationbird.cpp
@@ -32,36 +32,3 @@ int main() {
 }
 //niekxrtgfweucbjyfv
 
-#include <iostream>
-using namespace std;
-
-int migratoryBirds(int arr_count, int* arr) {
-    int freq[6] = {0}; // Index 1-5 for bird types
-
-    // Count frequency of each bird type
-    for (int i = 0; i < arr_count; i++) {
-        freq[arr[i]]++;
-    }
-
-    // Find the bird type with the maximum frequency
-    int max_freq = 0;
-    int bird_type = 0;
-
-    for (int i = 1; i <= 5; i++) {
-        if (freq[i] > max_freq) {
-            max_freq = freq[i];
-            bird_type = i;
-        }
-    }
-
-    return bird_type;
-}
-
-int main() {
-    int birds[] = {1, 1, 2, 2, 3, 3, 3, 4, 5, 4, 3};
-    int n = sizeof(birds) / sizeof(birds[0]);
-    int result = migratoryBirds(n, birds);
-    cout << "Most frequent bird type: " << result << endl;
-    return 0;
-}
-//niekxrtgfweucbjyfv
--- sum_of_arr_2_element_divisible_by_k.cpp
+++ sum_of_arr_2_element_divisible_by_k.cpp
@@ -8,14 +8,3 @@ int divisibleSumPairs(int n, int k, int ar_count, int* ar) {
 
 }
 //bycrfgnbrsjxfgb
-
-int divisibleSumPairs(int n, int k, int ar_count, int* ar) {
-    int count =0;
-    for (int i = 0 ;i<n;i++){
-        for(int j =i+1 ; j<n;j++){
-            if ((ar[i]+ar[j]) % k ==0  ) count +=1;
-        }
-    }return count ;
-
-}
-//bycrfgnbrsjxfgb
